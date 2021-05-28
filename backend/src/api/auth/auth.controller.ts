658fed3e004edb69dccedf23b3fbc1582aa1001f
Julio R <cesar32az@gmail.com>
2021-05-27 21:14:19 -0600

    login realizado, falta jwt

--- backend/src/api/auth/auth.controller.ts
+++ backend/src/api/auth/auth.controller.ts
@@ -20,6 +20,13 @@ export const register = async (req: Request, res: Response): Promise<Response> =
 
 export const login = async (req: Request, res: Response): Promise<Response> => {
   try {
+    const { email, password } = req.body;
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne({ where: { email } });
+    if (!user) return res.status(400).json({ message: 'usuario no encontrado' });
+    const match = await comparePassword(password, user.password);
+    if (!match) return res.status(400).json({ message: 'credenciales invalidas' });
+
     return res.status(200).json({ message: 'ok' });
   } catch (error) {
     console.log(error);
